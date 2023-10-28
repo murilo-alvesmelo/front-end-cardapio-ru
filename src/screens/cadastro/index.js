dc35364899453d13b4440c1a34cdb22f9d2ce9a5
murilo-alvesmelo <murilo.melo72@gmail.com>
2023-10-28 20:57:04 -0300

    feature/implementando login e cadastro

--- src/screens/cadastro/index.js
+++ src/screens/cadastro/index.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import './styles.css';
+import "./styles.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
 
 function Cadastro() {
-    return (
-        <div className="container">
-            <div className="form-group">
-                <img src={process.env.PUBLIC_URL + '/assets/brasaoUFT.png'} alt="Logo UFT" />
+  const [nome, setNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-                <input type="text" placeholder="Nome" />
-                <input type="text" placeholder="CPF" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-                <div>
-                    <button className="login">Login</button>
-                    <button className="cadastrar">Cadastrar</button>
-                </div>
-            </div>
+    if (nome === "" || cpf === "" || email === "" || password === "")
+      return alert("Preencha todos os campos");
 
+    if (nome && cpf && email && password) {
+      await api
+        .post("/signup", {
+          name: nome,
+          cpf: cpf,
+          email: email,
+          password: password,
+        })
+        .then(() => {
+          alert("Cadastro realizado com sucesso!");
+          navigate("/");
+        })
+        .catch((error) => {
+          alert("Erro ao cadastrar");
+          console.log(error);
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <form className="form-group" onSubmit={handleSubmit}>
+        <img
+          src={process.env.PUBLIC_URL + "/assets/brasaoUFT.png"}
+          alt="Logo UFT"
+        />
+
+        <input
+          required={true}
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input
+          required={true}
+          type="text"
+          placeholder="CPF"
+          value={cpf}
+          onChange={(e) => setCpf(e.target.value)}
+        />
+        <input
+          required={true}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          required={true}
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div>
+          <button className="login" onClick={() => navigate("/")}>
+            Login
+          </button>
+          <button className="cadastrar">Cadastrar</button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default Cadastro;
