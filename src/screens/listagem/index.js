85ad54e02a0e718a12b130d730afd0c8e3ba5339
Thomas <impedrothomas@outlook.com>
2023-11-04 17:17:19 -0300

    finalizando a lsitagem

--- src/screens/listagem/index.js
+++ src/screens/listagem/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importar useNavigate
 import './styles.css';
 
 function Listagem() {
     const [refeicoes, setRefeicoes] = useState([]);
+    const navigate = useNavigate(); // Inicializar useNavigate
 
     useEffect(() => {
         fetch('http://localhost:5000/cardapio')
@@ -10,6 +12,11 @@ function Listagem() {
         .then(data => setRefeicoes(data))
         .catch(error => console.error('Erro ao buscar dados:', error));
     }, []);
+
+    // Função para lidar com o clique na linha da tabela
+    const handleRowClick = (id) => {
+        navigate(`/refeicao/${id}`); // Usar a função navigate para redirecionar
+    };
 
     return (
         <div className="lista-refeicoes">
@@ -24,11 +31,12 @@ function Listagem() {
                 </thead>
                 <tbody>
                     {refeicoes.map(refeicao => (
-                    <tr key={refeicao.id}>
-                        <td>{new Date(refeicao.estimateAt).toLocaleDateString('pt-BR')}</td>
-                        <td>{new Date(refeicao.estimateAt).toLocaleDateString('pt-BR', { weekday: 'long' })}</td>
-                        <td>{refeicao.refeicao}</td>
-                    </tr>
+                        // Adicionando o evento onClick para cada linha da tabela
+                        <tr key={refeicao.id} onClick={() => handleRowClick(refeicao.id)} style={{ cursor: 'pointer' }}>
+                            <td>{new Date(refeicao.estimateAt).toLocaleDateString('pt-BR')}</td>
+                            <td>{new Date(refeicao.estimateAt).toLocaleDateString('pt-BR', { weekday: 'long' })}</td>
+                            <td>{refeicao.refeicao}</td>
+                        </tr>
                     ))}
                 </tbody>
             </table>
